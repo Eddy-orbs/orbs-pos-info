19b2280e9bdda877a7ae75672a5a269047c2b77f
Eddy Kim <56513320+Eddy-orbs@users.noreply.github.com>
2021-05-20 14:36:49 +0900

    Create API.java

---
+++ app/src/main/java/com/orbs/info/api/API.java
@@ -0,0 +1,18 @@
+package com.orbs.info.api;
+
+public class API {
+    public static final String INFURA_END_POINT = "https://mainnet.infura.io/v3/YOUR-OWN-INFURA-KEY"; // infura key
+    public static final String CONTRACT_STAKE = "0x01D59Af68E2dcb44e04C50e05F62E7043F2656C3";
+
+    public static final String ETHERSCAN_API_URL = "https://api.etherscan.io/api";
+    public static final String ETHERSCAN_API_KEY = "YOUR-OWN-ETHERSCAN-API-KEY"; // etherscan key
+    public static final String CONTRACT_STAKE_TOPIC_STAKE = "0x1449c6dd7851abc30abf37f57715f492010519147cc2652fbc38202c18a6ee90";
+    public static final String CONTRACT_STAKE_TOPIC_UNSTAKE = "0x7fc4727e062e336010f2c282598ef5f14facb3de68cf8195c2f23e1454b2b74e";
+    public static final String CONTRACT_STAKE_TOPIC_WITHDREW = "0xadec52fcd1408589179b85e44b434374db078b4eaf793e7d1a1bb0ae4ecfeee5";
+    public static final String CONTRACT_STAKE_TOPIC_RESTAKE = "0xa217c421e0e9357b7b1815d752952b142ddc0e23f9f14ecb8233f8f83d563c4d";
+
+    public static final String PRICE_ORBS_API_URL = "https://min-api.cryptocompare.com/data/price?fsym=ORBS&tsyms=USD,JPY,KRW";
+    public static final String PRICE_ETH_API_URL = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,JPY,KRW";
+    public static final String TOKEN_PRICE_API_URL = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ORBS,ETH&tsyms=USD,KRW,JPY";
+    public static final String ORBS_WEB_API_URL = "http://status.orbs.network/json";
+}
